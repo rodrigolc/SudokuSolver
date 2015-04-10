bddc31ff85ae2615505a4d8020e8821939cbe817
vvanessa <vvanessa@mail.uoguelph.ca>
2015-04-09 21:12:34 -0400

    add sudoku + basic app

---
+++ sudokuApp/sudokuApp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'sudokuApp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # url(r'^$', include(urls)),
+]
